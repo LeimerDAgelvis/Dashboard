7e71d62a4740e4b4388d5c0a38d964265505cbe3
leimer02 <leimer.agelvis@gmail.com>
2022-03-11 16:47:06 -0300

    1parte...

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopBar from './Topbar';
+
+function Header(){
+    return (
+        <React.Fragment>
+                <div id="content">
+                    <TopBar/>
+                </div>
+        </React.Fragment>
+    )
+}
+export default Header;
