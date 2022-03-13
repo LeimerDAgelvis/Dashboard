997cddf01bbb7e2c1c6a12cef8e14d009323cc63
Nicogonzap <nicolasgonzap@gmail.com>
2022-03-13 13:06:50 -0300

    agregado vínculo con BBDD para tabla

--- src/component/TablaContent.js
+++ src/component/TablaContent.js
@@ -1,64 +1,28 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import React from 'react'
 import TablaRow from "./TablaRow";
 
-let GabineteAT = {
-    id: 1, 
-    nombre: "Gabinete AT",
-    precio: 3889,
-    stock: 12,
-    vendidos: 2
-}
 
-let TecladoGenius = {
-    id: 2, 
-    nombre: "Teclado Genius",
-    precio: 689,
-    stock: 50,
-    vendidos: 12
-}
-
-let MouseLogitech = {
-    id: 3, 
-    nombre: "Mouse Logitech",
-    precio: 2544,
-    stock: 88,
-    vendidos: 25
-}
-
-let MonitorSamsung = {
-    id: 4, 
-    nombre: "Monitor Samsung 29 pulgadas",
-    precio: 23222,
-    stock: 3,
-    vendidos: 1
-}
-
-let LaptopLenovo= {
-    id: 5, 
-    nombre: "Laptop Lenovo Thinkpad I9",
-    precio: 355447,
-    stock: 2,
-    vendidos: 0
-}
-
-let MotherboardASUS = {
-    id: 1, 
-    nombre: "Motherboard ASUS Gaming",
-    precio: 13349,
-    stock: 12,
-    vendidos: 2
-}
-
-const dataProductos = [GabineteAT, TecladoGenius, MouseLogitech, MonitorSamsung, LaptopLenovo, MotherboardASUS];
-
+//const dataProductos = [GabineteAT, TecladoGenius, MouseLogitech, MonitorSamsung, LaptopLenovo, MotherboardASUS];
 
 function TablaContent(){
+    const [productos, setProductos] = useState([])
+    useEffect(()=>{
+        fetch('/api/products')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.data)
+            setProductos(data.data);
+        })
+        .catch(err => console.log(err))
+
+    },[])
     return (
         <React.Fragment>
             {
-                dataProductos.map((data,index)=>{
+                productos.map((producto)=>{
                     return (
-                        <TablaRow {...data} key={data.id} />                      
+                        <TablaRow product={producto} key={producto.id} />                      
                         )
                 })
             }
