8661588dee9254a75d8feb1ab1ab61e3c032dc6a
Luis <malpeliluis@gmail.com>
2022-03-13 15:53:43 -0300

    contentTotales

--- src/component/ContenTop.js
+++ src/component/ContenTop.js
@@ -1,62 +1,38 @@
 import React from "react";
 import ContentTotales from './ContentTotales'
-
-
-let Usuarios = {
-    color:   "primary",
-    titulo: "Total Usuarios",
-    valor: 500,
-    icono: "fas fa-film",
-}
-
-let Categorias ={
-    color:   "success",
-    titulo: "Total Categorias",
-    valor: 50,
-    icono: "fas fa-award",
-}
-
-let Productos = {
-    color:   "warning",
-    titulo: "Total Productos",
-    valor: 100,
-    icono: "fas fa-user",
-}
-
-let UltimoProducto = {
-    color:   "primary",
-    titulo: "Último producto creado",
-    valor: "Motherboard ASUS",
-    icono: "fas fa-film",
-}
-
-let UltimoUsuario ={
-    color:   "success",
-    titulo: "Último usuario creado",
-    valor: "Mariano Gomez",
-    icono: "fas fa-award",
-}
-
-let ProductosGabinetes = {
-    color:   "warning",
-    titulo: "Total de Productos en Gabinetes",
-    valor: 12,
-    icono: "fas fa-user",
-}
-
-const BaseProps = [Usuarios,Categorias,Productos, UltimoProducto, UltimoUsuario, ProductosGabinetes];
-
+import { useState, useEffect } from 'react'
 
 function ContenTop(){
+
+    const [usuarios, setUsuarios] = useState([])
+    const [productos, setProductos] = useState([])
+    const [categorias, setCategorias] = useState([])
+
+    useEffect( () => {
+
+        fetch('/api/users')
+        .then(response => response.json())
+        .then(data => setUsuarios(data))
+
+        fetch('/api/products')
+        .then(response => response.json())
+        .then(data => setProductos(data))
+
+        fetch('/api/categories')
+        .then(response => response.json())
+        .then(data => setCategorias(data))
+
+    }, [])
+	
     return (
         <React.Fragment>
         {/*<!-- ContenTop -->*/}
         <div className="row padded-h">
-            {
-                BaseProps.map((Totales,index)=>{
-                    return <ContentTotales  {...Totales}  key= {index}/>
-                })
-            }      
+            <ContentTotales titulo='Usuarios' total={usuarios.count}/>
+
+            <ContentTotales titulo='Productos' total={productos.count}/>
+
+            <ContentTotales titulo='Categorias' total={categorias.count}/>     
         </div>
         </React.Fragment>
     )
