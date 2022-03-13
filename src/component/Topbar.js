f3aceb5a930aeccefdf5c4f3596c3a77c32ba34b
leimer02 <leimer.agelvis@gmail.com>
2022-03-13 14:22:55 -0300

    actualizar

--- src/component/Topbar.js
+++ src/component/Topbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import foto from '../assets/images/jordan-walke.png';
+import foto from '../assets/images/leimerdelgado.png';
 
 function Topbar(){
     return(
@@ -38,7 +38,7 @@ function Topbar(){
 						{/*<!-- Nav Item - User Information -->*/}
 						<li className="nav-item dropdown no-arrow">
 							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
-								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Jordan Walke</span>
+								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Leimer Delgado</span>
 								<img className="img-profile rounded-circle" src={foto} alt="Jordan Walke - Creador de React" width="60"/>
 							</a>
 						</li>
