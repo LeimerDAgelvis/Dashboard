4c6454c34f8841ec3dc9107a8d8476e40fd99c00
Nicogonzap <nicolasgonzap@gmail.com>
2022-03-14 19:45:09 -0300

    vinculo con back para edicion de producto

--- src/component/ProductEdit.js
+++ src/component/ProductEdit.js
@@ -1,39 +1,68 @@
-import React, {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import React, {useState, useEffect, useRef} from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
 
 function ProductoEdit(){
 
     const [Producto, setProducto] = useState({})
 
-    let { id } = useParams()
+    let { id } = useParams();
+    let navigate = useNavigate();
 
     useEffect( () => {
         fetch(`/api/products/${id}`)
         .then(response => response.json())
-        .then(data => setProducto(data))
-    }, [])
+        .then(data => {
+            setProducto(data)})
+    }, []);
+
+    const title = useRef()
+    const description = useRef();
+    const model = useRef();
+    const img = useRef();
+    const price = useRef();
+
+    function postToBackEnd (e) {
+        e.preventDefault();
+        fetch(`/api/products/edit/${Producto.id}?_method=PUT`,{
+            method:'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'},
+            body:JSON.stringify({
+                title:title.current.value,
+                description:description.current.value,
+                model:model.current.value,
+                img:img.current.value,
+                price:price.current.value
+            })
+        })
+        .then(navigate('/'));
+        
+    }
 
     return (
+        
         <div className="card shadow position-top-box">
             <h3>{Producto.title}</h3>
             <br/>
-            <form className="productoBox" action={`/api/products/edit/${Producto.id}?_method=PUT`} method="POST" encType="multipart/form-data" id="formulario">
+            <form className="productoBox" action={`/api/products/edit/${Producto.id}?_method=PUT`} method="POST" encType="multipart/form-data" id="formulario" onSubmit={postToBackEnd}>
                 <span>Nombre del producto: </span> 
                 <br/>
-                <input type="text" name= "title" className="input-producto" placeholder={Producto.title}/>
+                <input type="text" name= "title" className="input-producto" placeholder={Producto.title} ref={title}/>
                 <br/>
                 <span>Descripcion del producto: </span>
                 <br/>
-                <textarea className="input-producto" rows= "10" cols="50" name= "description" placeholder={Producto.description}></textarea>
+                <textarea className="input-producto" rows= "10" cols="50" name= "description" placeholder={Producto.description} ref={description}></textarea>
                 <br/>
                 <span>Modelo: </span>
                 <br/>
-                <input type="text" name="model" className="input-producto" placeholder={Producto.model}/>                                                  
+                <input type="text" name="model" className="input-producto" placeholder={Producto.model} ref={model}/>                                                  
                 <br/>
                 <span>Imagen: </span>
                 <br/>
                 <div><img src={`/img/productos/${Producto.img}`} alt={Producto.title}/></div>
-                <input className="img" type="file" name= "img" />
+                <input className="img" type="file" name= "img" ref={img} />
                 <br/>
                 <span>Categoria:</span>
                 <br/>
@@ -68,7 +97,7 @@ function ProductoEdit(){
                 <br/>
                 <span>Precio: </span>
                 <br/>
-                <input type="text" name="price" className="input-producto" placeholder={Producto.price}/>                                                  
+                <input type="text" name="price" className="input-producto" placeholder={Producto.price} ref={price}/>                                                  
                 <br/>
                 <div className="botones-div"> 
                     <button type="submit" className="boton boton-editar">Guardar</button>
