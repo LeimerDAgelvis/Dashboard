b218f6ff67898d2f55b01db90168a865375e0856
leimer02 <leimer.agelvis@gmail.com>
2022-03-18 09:02:15 -0300

    error

--- src/component/Enrutador.js
+++ src/component/Enrutador.js
@@ -7,16 +7,19 @@ import Categorias from './Categorias'
 import ProductoDetail from './DetalleProductos/ProductoDetail'
 import ProductoCreate from './DetalleProductos/ProductoCreate'
 import ProductoEdit from './DetalleProductos/ProductEdit'
+import Error404 from './Error404'
+
 function Enrutador () {
     return (
         <Routes>
-            <Route path='/' element={<ContentWrapper/>}/>
+            <Route exact path='/' element={<ContentWrapper/>}/>
             <Route path='/usuarios' element={<Usuarios/>}/>
             <Route path='/products' element={<Tabla/>}/>
             <Route path='/categorias' element={<Categorias/>}/>
             <Route path='/products/:id' element={<ProductoDetail/>}/>
             <Route path='/products/edit/:id' element={<ProductoEdit/>}/>
             <Route path='/products/crear' element={<ProductoCreate/>}/>
+            <Route path='*' element={<Error404/>}/>
         </Routes>
     )
 }
---
+++ src/component/Error404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Error404 () {
+    return(
+        <div style={{'color':'red'}}>
+            <h1>ERROR 404 - Ruta no encontrada</h1>
+        </div>
+    )
+}
+
+export default Error404
